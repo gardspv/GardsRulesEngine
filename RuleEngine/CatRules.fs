360d0c519498c949016d95447cd243121c0a5c0f
gardspv <121796506+gardspv@users.noreply.github.com>
2023-04-13 14:41:59 +0200

    Add files via upload

---
+++ RuleEngine/CatRules.fs
@@ -0,0 +1,20 @@
+﻿module RuleEngine.CatRules
+
+open RuleEngine
+
+let catRules =
+    [
+        {Description="Cat must not be too old"
+         RedLightRule=Some({Reason="Value > 10";
+                            Rule=(fun x -> x.Age > 10)})
+         YellowLightRule=Some({Reason="Value > 6"
+                               Rule=(fun x -> x.Age > 6)})
+         }
+        {Description="Cat must not be black"
+         RedLightRule=Some({Reason="Cat is black";
+                            Rule=(fun x -> x.Color.Equals("Black"))})
+         YellowLightRule=None
+         }
+    ]
+let checkBusinessObject businessObject =
+    checkRulesForBusinessObject catRules businessObject
---
+++ RuleEngineCalledV2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RuleEngineCalledV2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var cat = new RuleEngine.RuleEngine.Cat("grey", 17.1, 11);
+        var result = RuleEngine.CatRules.checkBusinessObject(cat);
+        Console.WriteLine(result);
+    }
+}
