c575e45d76efe702a65f8188af7d70817473d341
Gard Askeland <gard.askeland@spv.no>
2023-04-14 15:36:52 +0200

    Rudis kommentarer

--- RuleEngine/RuleEngine.fs
+++ RuleEngine/RuleEngine.fs
@@ -15,25 +15,45 @@ type Cat =
       Weight: double
       Age: int }
 
-type Rule<'T> = { Reason: string; Rule: ('T -> bool) }
+type Rule<'T> = { Criteria: string
+                  Rule: 'T -> bool }
+
+type LabelledRule<'T> =
+    Red of Rule<'T>
+    | Yellow of Rule<'T>
+
+let trafficLightComparator x y =
+    match x with
+    | Red _ ->
+        match y with
+        | Yellow _ -> -1
+        | Red _ -> 0
+    | Yellow x ->
+        match y with
+        | Yellow _ -> 0
+        | Red _ -> 1
+    
 
 type RuleSet<'T> =
     { Description: string
-      RedLightRule: Rule<'T> option
-      YellowLightRule: Rule<'T> option }
+      Rules: List<LabelledRule<'T>> }
 
-type RuleResult =
+type RuleResult<'T> =
     { RuleDescription: string
       Light: TrafficLight
-      Reason: string }
+      CriteriaViolated: string
+    }
 
-let createRuleResult ruleSetDescription ruleReason light =
+let createRuleResult ruleSetDescription ruleCriteria light =
     { RuleDescription = ruleSetDescription
       Light = light
-      Reason = ruleReason }
-
+      CriteriaViolated = ruleCriteria }
+    
+//Iterere gjennom listen, avslutte om en regel brytes
 
 let evaluate businessObject ruleSet =
+    let sortedRuleSet = List.sortWith trafficLightComparator ruleSet
+    
     if ruleSet.RedLightRule.IsSome && ruleSet.RedLightRule.Value.Rule businessObject then
         createRuleResult ruleSet.Description ruleSet.RedLightRule.Value.Reason Red
     elif ruleSet.YellowLightRule.IsSome && ruleSet.YellowLightRule.Value.Rule businessObject then
